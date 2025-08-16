62918e7f57d3659a954a6ff8bc77ce1c23b6b779
ilovezerocokeya <98097563+ilovezerocokeya@users.noreply.github.com>
2025-08-17 01:19:01 +0900

    (WHI-24) fix: button.stories.tsx파일 경로 대문자 수정
    기존의 대문자로 작성되었던 경로를 소문자로 변경했습니다.

--- src/shared/ui/button/button.stories.tsx
+++ src/shared/ui/button/button.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Button } from './button';
 
 const meta = {
-  title: 'Shared/Button',
+  title: 'shared/Button',
   component: Button,
   tags: ['autodocs'],
   args: {
