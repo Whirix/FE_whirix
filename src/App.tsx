1d0ff2f898cac4d53a8481a362da9478347fb838
minkonkim <alsrhs98@gmail.com>
2025-08-30 12:57:22 +0900

    WHI-26  chore : 메인 화면 초기상태로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import { Input } from './shared/ui/input/input'
 
 function App() {
   return (
-    <>
-      <Input />
-    </>
+    <div className="flex flex-col gap-[1vh] bg-amber-300 p-5">
+      <Input label="" placeholder="Default" />
+      <Input label="with Label" placeholder="WithLabel" />
+      <Input maxLength={10} placeholder="maxLength 10" />
+      <Input fullWidth placeholder="Full Width" />
+      <Input required placeholder="Required" />
+      <Input errorText="입력해주세요" placeholder="Error Text" />
+      <Input showCounter placeholder="Counter" maxLength={12} />
+    </div>
   )
 }
 
