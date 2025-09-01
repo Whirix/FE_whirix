234d3e36a11acbdbd77a3e97078384a509328ed8
minkonkim <alsrhs98@gmail.com>
2025-09-01 15:38:16 +0900

    WHI-27 chore:  스토리북 빌드 최적화

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,5 +16,40 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
+  async viteFinal(config, { configType }) {
+    if (configType === 'PRODUCTION') {
+      if (config.build) {
+        config.build.rollupOptions = {
+          output: {
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                if (id.includes('axe-core')) {
+                  return 'axe-core'
+                }
+                if (id.includes('@floating-ui/react')) {
+                  return 'floating-ui'
+                }
+                if (id.includes('react-dom')) {
+                  return 'react-dom'
+                }
+                if (id.includes('react')) {
+                  return 'react'
+                }
+                return 'vendor' // 나머지 node_modules
+              }
+
+              // 로컬 컴포넌트 청킹
+              const componentMatch = id.match(/src\/(shared\/ui|feature)\/([^/]+)\/index\.ts/)
+              if (componentMatch) {
+                return `component-${componentMatch[2]}`
+              }
+            },
+          },
+        }
+        config.build.chunkSizeWarningLimit = 600
+      }
+    }
+    return config
+  },
 }
 export default config
