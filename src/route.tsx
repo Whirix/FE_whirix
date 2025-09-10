adde6170f08d2c7b82d3ed45ac0e5e9e93feac3e
kybaq <flfsk12@gmail.com>
2025-09-10 13:36:34 +0900

    WHI-31 feat: /game route 추가

--- src/route.tsx
+++ src/route.tsx
@@ -1,16 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import Home from './pages/home'
+import Game from './pages/game'
 import { Error } from './shared/components/error'
+import { Layout } from './shared/layout/layout'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
+    element: <Layout />,
     errorElement: <Error />,
     children: [
       { index: true, element: <Home /> }, // path="/" 와 동일
       { path: 'home', element: <Home /> }, // path="/home" 와 동일
+      { path: 'game', element: <Game /> },
     ],
   },
 ])
